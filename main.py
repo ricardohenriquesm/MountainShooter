803b610fc8542bba5300becd6ef0e300a63e0a9e
ric <ricardo2002.henrique@gmail.com>
2026-04-06 11:50:20 -0300

    Adding Classes generated from StarUML

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 window = pygame.display.set_mode(size=(600, 480))
 print("Setup End")
 
-print(("Loop Start"))
+print("Loop Start")
 while True:
     # Check for all events
     for event in pygame.event.get():
