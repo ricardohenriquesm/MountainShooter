9f282a51c7d3cc8f2452fd30a84ad75e52517331
ric <ricardo2002.henrique@gmail.com>
2026-04-10 11:37:11 -0300

    finish

--- code/Const.py
+++ code/Const.py
@@ -138,10 +138,28 @@ PLAYER_KEY_SHOT = {'Player1': pygame.K_RCTRL,
 # S
 SPAWN_TIME = 2000
 
+
 # T
 TIMEOUT_STEP = 100
 
-TIMEOUT_LEVEL = 20000
+TIMEOUT_LEVEL = 5000
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 326
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
--- code/Game.py
+++ code/Game.py
@@ -3,6 +3,7 @@ import pygame
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -11,6 +12,7 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
     def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -21,6 +23,14 @@ class Game:
                 if level_return == True:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
+
+
+
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
